1abc42520e55a57e385b1351864db8ea9bb8b7af
JakubSQ <skuta.jakub@gmail.com>
2022-04-25 14:04:46 +0200

    table and model for trips created

---
+++ db/migrate/20220319164010_create_trips.rb
@@ -0,0 +1,10 @@
+class CreateTrips < ActiveRecord::Migration[7.0]
+  def change
+    create_table :trips do |t|
+      t.string :start_address
+      t.string :destination_address
+      t.decimal :price, precision: 8, scale: 2
+      t.timestamps
+    end
+  end
+end
