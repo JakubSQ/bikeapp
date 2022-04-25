2edde783ef1cef4852fddc8f49218958a57e514c
JakubSQ <skuta.jakub@gmail.com>
2022-04-25 14:42:42 +0200

    api key moved to rails credentials from service

--- app/services/trips/creator.rb
+++ app/services/trips/creator.rb
@@ -16,7 +16,10 @@ module Trips
 
       origin = params[:start_address].gsub(/[., '"]/,'')
       destination = params[:destination_address].gsub(/[., '"]/,'')
-      response = HTTParty.get(url = URI("https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{origin}&destinations=#{destination}&key=AIzaSyBqM7lt_BMFP4PHRlGUqqCurdPlJ5AQ6xA&language=pl&region=PL"))
+      
+      binding.pry
+      
+      response = HTTParty.get(url = URI("https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{origin}&destinations=#{destination}&key=#{Rails.application.credentials.google_distance[:secret_access_key]}&language=pl&region=PL"))
       @trip.distance = (response["rows"][0]["elements"][0]["distance"]["value"]/1000.to_f).ceil
       @trip.save
     end
